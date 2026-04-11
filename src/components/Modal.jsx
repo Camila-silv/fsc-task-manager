bddca24b34e449278a2fb281446608fc607070dd
Camila Silva <cf.silv466@gmail.com>
2026-04-11 12:01:27 -0300

    feat: adicionado hook form ao projeto

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,68 +1,24 @@
 // import { AnimatePresence, motion } from "framer-motion";
 import PropTypes from "prop-types";
-import { useRef, useState } from "react";
 import { createPortal } from "react-dom";
+import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { v4 as uuid } from "uuid";
 
 import { LoaderIcon } from "../assets/icons";
-import {
-  AlertMessage,
-  Button,
-  Input,
-  Label,
-  TimeSelect,
-} from "../components/index";
+import { Button, Input, Label, TimeSelect } from "../components/index";
 
-const Modal = ({
-  handleShowModal,
-  handleTasks,
-  isLoading,
-  handleIsLoading,
-}) => {
-  const titleRef = useRef();
-  const timeRef = useRef();
-  const descriptionRef = useRef();
+const Modal = ({ handleShowModal, handleTasks }) => {
+  const {
+    register,
+    formState: { errors, isSubmitting },
+    handleSubmit,
+  } = useForm();
 
-  const [errors, setErrors] = useState([]);
-
-  const addTask = async (e) => {
-    e.preventDefault();
-    handleIsLoading(true);
-
-    const newErrors = [];
-
-    const title = titleRef.current.value;
-    const time = timeRef.current.value;
-    const description = descriptionRef.current.value;
-
-    if (!title.trim()) {
-      newErrors.push({
-        inputName: "title",
-        message: "Digite um titulo válido.",
-      });
-    }
-
-    if (!time.trim()) {
-      newErrors.push({
-        inputName: "time",
-        message: "Selecione um horário.",
-      });
-    }
-
-    if (!description.trim()) {
-      newErrors.push({
-        inputName: "description",
-        message: "Digite uma descrição válida.",
-      });
-    }
-
-    setErrors(newErrors);
-
-    if (newErrors.length > 0) {
-      handleIsLoading(false);
-      return;
-    }
+  const addTask = async (data) => {
+    const title = data.title.trim();
+    const time = data.time.trim();
+    const description = data.description.trim();
 
     const newTask = {
       id: uuid(),
@@ -90,18 +46,9 @@ const Modal = ({
       return [...tasks, newTask];
     });
 
-    titleRef.current.value = "";
-    descriptionRef.current.value = "";
     toast.success("Tarefa adicionada com sucesso.");
-    handleIsLoading(false);
     handleShowModal(false);
   };
-
-  const titleError = errors.find((error) => error.inputName === "title");
-  const descriptionError = errors.find(
-    (error) => error.inputName === "description"
-  );
-  const timeError = errors.find((error) => error.inputName === "time");
 
   return (
     <>
@@ -109,7 +56,7 @@ const Modal = ({
         <div className="fixed top-0 left-0 z-40 flex h-full w-full items-center justify-center bg-[#09090B1F]">
           <form
             className="flex w-full max-w-84 flex-col gap-4 rounded-xl bg-white p-5"
-            onSubmit={addTask}
+            onSubmit={handleSubmit(addTask)}
           >
             <div className="flex flex-col items-center gap-1">
               <h2 className="text-brand-dark-blue text-center text-[20px] font-semibold">
@@ -121,33 +68,60 @@ const Modal = ({
               </h3>
             </div>
 
-            <div className="flex flex-col gap-1">
+            <Input
+              name="title"
+              id="title"
+              placeholder="Título da tarefa"
+              disabled={isSubmitting}
+              {...register("title", {
+                required: "O título é obrigatório.",
+                validate: (value) => {
+                  if (!value.trim()) {
+                    return "O título não pode ser vázio.";
+                  }
+
+                  return true;
+                },
+              })}
+              error={errors?.title?.message}
+            >
               <Label name="title" title="Título" />
-              <Input
-                name="title"
-                id="title"
-                placeholder="Título da tarefa"
-                ref={titleRef}
-                disabled={isLoading}
-              />
-              {titleError && <AlertMessage>{titleError.message}</AlertMessage>}
-            </div>
+            </Input>
 
-            <TimeSelect error={timeError} ref={timeRef} disabled={isLoading} />
+            <TimeSelect
+              error={errors?.title?.message}
+              disabled={isSubmitting}
+              {...register("time", {
+                required: "O período é obrigatório.",
+                validate: (value) => {
+                  if (!value.trim()) {
+                    return "Selecione um período.";
+                  }
 
-            <div className="flex flex-col gap-1">
+                  return true;
+                },
+              })}
+            />
+
+            <Input
+              name="description"
+              id="description"
+              placeholder="Descreva a tarefa"
+              disabled={isSubmitting}
+              {...register("description", {
+                required: "A descrição é obrigatório.",
+                validate: (value) => {
+                  if (!value.trim()) {
+                    return "A descrição não pode ser vázia.";
+                  }
+
+                  return true;
+                },
+              })}
+              error={errors?.description?.message}
+            >
               <Label name="description" title="Descrição" />
-              <Input
-                name="description"
-                id="description"
-                placeholder="Descreva a tarefa"
-                ref={descriptionRef}
-                disabled={isLoading}
-              />
-              {descriptionError && (
-                <AlertMessage>{descriptionError.message}</AlertMessage>
-              )}
-            </div>
+            </Input>
 
             <div className="flex items-center gap-3">
               <Button
@@ -164,9 +138,9 @@ const Modal = ({
                 size="large"
                 className="w-full"
                 type="submit"
-                disabled={isLoading}
+                disabled={isSubmitting}
               >
-                Salvar {isLoading && <LoaderIcon className="animate-spin" />}
+                Salvar {isSubmitting && <LoaderIcon className="animate-spin" />}
               </Button>
             </div>
           </form>
@@ -182,6 +156,4 @@ export default Modal;
 Modal.propTypes = {
   handleShowModal: PropTypes.func,
   handleTasks: PropTypes.func,
-  handleIsLoading: PropTypes.func,
-  isLoading: PropTypes.bool,
 };
